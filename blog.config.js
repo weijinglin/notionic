9736884329f074d67e354876b2f1ecbc59a245dd
jinglinwei <weijinglin@sjtu.edu.cn>
2024-12-28 21:10:11 +0800

    fix: update blog configuration with new title, author, and description

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
-  link: 'https://zuolan.me',
+  title: "Linyu's Light Years",
+  author: 'Linyu',
+  email: 'wellion77777@gmail.com',
+  link: 'https://linyu.me',
   newsletter: 'Notionic Weekly',
-  description: 'A static blog build on top of Notion and Next.js',
+  description: 'Yesterday is a history, tomorrow is a mystery, only today is a gift',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
